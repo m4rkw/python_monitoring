ae92f7a56e70fcb3697322b64b6ebe1824f213e5
Mark Wadham <m@rkw.io>
2025-03-07 19:28:43 +0000

    update

--- cleanup_layers.py
+++ cleanup_layers.py
@@ -4,12 +4,14 @@ import os
 import sys
 import json
 
+LAYER = "lambda_monitoring"
+
 def cleanup_old_versions(layer_name, versions_to_delete):
     for version in versions_to_delete:
         print(f"aws lambda delete-layer-version --layer-name {layer_name} --version-number {version['Version']}")
         os.system(f"aws lambda delete-layer-version --layer-name {layer_name} --version-number {version['Version']}")
 
-versions = json.loads(os.popen(f"aws lambda list-layer-versions --layer-name lambda_monitoring").read())
+versions = json.loads(os.popen(f"aws lambda list-layer-versions --layer-name {LAYER}").read())
 
 if len(versions['LayerVersions']) >1:
-    cleanup_old_versions(layer['LayerName'], versions['LayerVersions'][1:])
+    cleanup_old_versions(LAYER, versions['LayerVersions'][1:])
